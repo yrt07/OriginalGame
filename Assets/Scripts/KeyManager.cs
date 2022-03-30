202e3abd08404fb539853d819439fb19a6459197
yrt07 <y7.bike@gmail.com>
2022-03-30 16:52:41 +0900

    編集

---
+++ Assets/Scripts/KeyManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyManager : MonoBehaviour
+{
+    [SerializeField] Image[] Keyimage;
+
+    public int keyNum;
+    public void addkey()
+    {
+        Debug.Log("addkey");
+
+        keyNum++;
+        showkey(keyNum);
+
+    }
+    private void showkey(int num)
+    {
+        for (int i = 0; i < 1;)
+        {
+            if (i < num)
+            {
+                Keyimage[i].color = Color.white;
+
+            }
+            else
+            {
+                Keyimage[i].color = Color.black;
+
+            }
+
+        }
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        showkey(keyNum);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
